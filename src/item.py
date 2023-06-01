349b1ba3a4702ac1f9c2df0b92240abedab29f14
eliseev <twicheg@gmail.com>
2023-06-01 18:26:56 +0300

    complete home-word 13.1

--- src/item.py
+++ src/item.py
@@ -2,29 +2,23 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
+    pay_rate = 0.85
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+    def __init__(self, name, price, quantity):
+        """ Создание экземпляра класса item. """
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def calculate_total_price(self):
 
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        """ Рассчитывает общую стоимость конкретного товара в магазине.
+        :return: Общая стоимость товара."""
+        return self.price * self.quantity
 
-        :return: Общая стоимость товара.
-        """
-        pass
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+    def apply_discount(self):
+        """ Применяет установленную скидку для конкретного товара. """
+        self.price *= Item.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 1000, 2)
+
+
+def test_item(item):
+    assert item.name == "Смартфон"
+    assert item.price == 1000
+    assert item.quantity == 2
+    assert item.calculate_total_price() == 2000
+    assert item.apply_discount() == None
