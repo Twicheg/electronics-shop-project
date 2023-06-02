ab1b39392906e21d88b2702da786e0d8d97998d3
eliseev <twicheg@gmail.com>
2023-06-02 18:53:20 +0300

    some add to test

--- tests/test_item.py
+++ tests/test_item.py
@@ -12,4 +12,7 @@ def test_item(item):
     assert item.price == 1000
     assert item.quantity == 2
     assert item.calculate_total_price() == 2000
-    assert item.apply_discount() == None
+    item.apply_discount()
+    assert item.price == 1000 * 0.85
+    item.apply_discount()
+    assert item.price == 1000 * 0.85 * 0.85
