374098ae30ce573b00c322b8ad536bc45f7e4785
eliseev <twicheg@gmail.com>
2023-06-17 12:07:48 +0300

    add super()

--- src/item.py
+++ src/item.py
@@ -57,9 +57,6 @@ class Item:
         else:
             return self.quantity + other
 
-    # def __radd__(self, other):
-    #     return other.quantity + self.quantity
-
     def __repr__(self):
         """Метод отображения инфаормации в режиме отладки"""
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
