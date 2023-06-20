104948d79d0498938c74c2468c4a017400cff7bb
eliseev <twicheg@gmail.com>
2023-06-20 16:33:39 +0300

    hw5

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,36 +1,32 @@
 from src.item import Item
 
 
-class MixinLog:
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        super().__init__(name, price, quantity)
-        self.__language = 'EN'
-
+class Mixin:
     def change_lang(self):
-        if self.__language == 'EN':
-            self.__language = "RU"
+        if self._language == 'EN':
+            self._language = "RU"
             return self
-        else:
-            self.__language = 'EN'
+        elif self._language == "RU":
+            self._language = 'EN'
             return self
 
 
-class KeyBoard(Item, MixinLog):
+class KeyBoard(Mixin, Item):
     def __init__(self, name: str, price: float, quantity: int) -> None:
         super().__init__(name, price, quantity)
-        self.__language = 'EN'
+        self._language = 'EN'
 
     @property
     def language(self):
-        return self.__language
+        return self._language
 
-    # @language.setter
-    # def language(self, other_language):
-    #     """Функция по инициализации языка"""
-    #     if other_language in ["EN", "RU"]:
-    #         self.__language = other_language
-    #     else:
-    #         raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
+    @language.setter
+    def language(self, other_language):
+        """Функция по инициализации языка"""
+        if other_language in ["EN", "RU"]:
+            self._language = other_language
+        else:
+            raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
 
     def __str__(self):
         return f'{self.name}'
